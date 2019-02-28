fdefefc57dafb7c983b7d0d13d51b56bea4c5e88
scientist19 <40214489+scientist19@users.noreply.github.com>
2019-02-28 21:45:44 +0200

    added calcBestAns.cpp

---
+++ calcBestAns.cpp
@@ -0,0 +1,28 @@
+
+void updateAns(const vector <Slide*>& sls){
+	
+	vector <Slide*> sls0;
+	random_shuffle(sls0.begin(), sls0.end());
+	
+	int newAns = calculate_result(sls0);
+	if (newAns > bestScore) {
+		
+		bestScore = newAns;
+		answer = sls0;
+	}
+}
+
+void calcAns(const vector <Slide*>& sls){
+	
+	for (int i = 0; i < SLIDESHOW_PER_SET; i++)
+		updateAns(sls);
+}
+
+void calcBestAns(){
+	
+	for (int i = 0; i < SLIDES_SETS_COUNT; i++){
+		
+		getRandomSlides();
+		calcAns(slides);
+	}
+}
